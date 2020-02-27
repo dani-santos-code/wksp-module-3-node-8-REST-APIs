31f391c5d7dbd86f74770dd63cef211acac8013d
Dani Santos <santos.adaniele@gmail.com>
2020-02-27 16:18:59 -0500

    implement modal to show the right word I'm also fixing a bug that allowed the user to win if he/she guessed the last word correctly

--- public/hangman/scripts.js
+++ public/hangman/scripts.js
@@ -1,17 +1,18 @@
 // here I am extracting a random ID so, I can later use it for the guesses as well as generate divs
 
 const boxesWrapper = document.getElementById("boxes-wrapper");
-
+const modalDiv = document.getElementById("modal");
 const randomId = Math.round(Math.random() * (130 - 120) + 120).toString();
 
 const arrValues = new Array();
-
+const word = [];
 const generateDivs = async randomId => {
   const response = await fetch(`/hangman/${randomId}`);
   const { letterCount } = await response.json();
   for (i = 0; i < letterCount; i++) {
     const div = document.createElement("div");
     div.classList.add("letter-box");
+    div.setAttribute("id", i);
     boxesWrapper.appendChild(div);
   }
 };
@@ -26,23 +27,26 @@ const handleSubmit = async () => {
   if (trueIndexes.length !== 0) {
     for (let i of trueIndexes) {
       arrValues[i] = true;
+      word[i] = letter;
+      document.getElementById(i).innerText = letter;
     }
   }
-  // console.log(arrValues);
   checkWin(letterCount);
-  // if (checkWin()) {
-  //   console.log("YAY!!! YOU MADE IT!!");
-  // }
 };
 
 const checkWin = letterCount => {
   let isWinner = false;
+  console.log(arrValues);
   const isAllTrue = currentValue => currentValue === true;
-  if (arrValues.length === letterCount) {
+  if (arrValues.length === letterCount && !arrValues.includes(undefined)) {
     console.log("HERE. START CHECKING");
     isWinner = arrValues.every(isAllTrue);
   }
   if (isWinner) {
     console.log("YAY!!! YOU GOT IT!!!!!!!!");
+    const capitalizeFirst = word[0].toUpperCase();
+    const lowerCase = word.slice(1).join("");
+    modalDiv.style.display = "flex";
+    modalDiv.innerText = `${capitalizeFirst + lowerCase} it is! Woohooo!!!!!🌈`;
   }
 };
